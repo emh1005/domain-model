9f3cf5bb11c55e5deddc1d257c757e544c87fda2
emh <emh>
2016-10-17 23:54:10 -0700

    Initial Commit

---
+++ SimpleDomainModel/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SimpleDomainModel
+//
+//  Created by studentuser on 10/17/16.
+//  Copyright © 2016 emh. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
